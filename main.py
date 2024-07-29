a2e8d7226c3e50771f03ef4e0914b4722c162b32
Jay Ravalani <jay.ravalani746@gmail.com>
2024-07-30 00:42:42 +0530

    basic email template

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ import requests
 from send_mail import send_mail
 
 api_key = "fef8a28df1bb4e4fbcc7aead3ee95f82"
-url = f"https://newsapi.org/v2/top-headlines?country=in&apiKey={api_key}"
+url = f"https://newsapi.org/v2/top-headlines?country=in&apiKey={api_key}&language=en"
 
-request = requests.get(url)
-content = request.json()
+response = requests.get(url)
+content = response.json()
 
 titles = [article['title'] for article in content['articles']]
+top_10_titles = titles[:10]
 authors = [article['author'] for article in content['articles']]
+urls = [article['url'] for article in content['articles']]
 
 message = f"Subject: Daily News Update (Top Headlines)\n\nFrom: News API\n\n"
 
-for i in range(len(titles)):
+for i in range(len(top_10_titles)):
     author = authors[i] if authors[i] else "Unknown Author"
-    message += f"{i + 1}. {titles[i]} by {author}\n"
+    url = urls[i] if urls[i] else "No Link Available"
+    message += f"{i + 1}. {titles[i]} by {author}\n Link: {url}\n\n"
 
-print(message)
+message = message.encode("utf-8")
 
 send_mail(message)
